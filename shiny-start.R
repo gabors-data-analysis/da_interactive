8632649cf55f432deacfbaaa528c2464c6cc1b50
Gábor Békés <bekesg@gmail.com>
2021-01-26 16:19:55 +0100

    add sec

--- shiny-start.R
+++ shiny-start.R
@@ -5,7 +5,8 @@ library(rsconnect)
 
 rsconnect::setAccountInfo(name='gabors-data-analysis',
                           token='99969DF648E0532FDBAFC8040C025E4C',
-                          secret='<SECRET>')
+                          secret='rGF1D4lQ3rtKYaqWP+pNtHFGJ0gbq28rjdG3wj0c')
+
 
 library(rsconnect)
 rsconnect::deployApp('path/to/your/app'
